d31ab4ff98daf54b217564c78dad967af70fbc60
Qasim <qalhamad33@gmail.com>
2024-07-28 09:41:32 +0300

    template and foods controller

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name : { 
+    type: String ,
+    required : true
+  }
+});
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +15,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  food: [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -19,7 +23,9 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
+
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -42,7 +48,10 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+app.use(passUserToView)
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods',foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
